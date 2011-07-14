9df06b50b639cfcda3f9ace183e8a51ed58d1038
Aaron Day <daya-github@crazydays.org>
2011-07-13 20:55:50 -0400

    Added accelerometer and orientation sensors.

--- AndroidExploration/src/org/crazydays/android/sensor/SensorsActivity.java
+++ AndroidExploration/src/org/crazydays/android/sensor/SensorsActivity.java
@@ -2,6 +2,8 @@
 package org.crazydays.android.sensor;
 
 
+import java.text.MessageFormat;
+
 import org.crazydays.android.R;
 
 import android.app.Activity;
@@ -25,6 +27,18 @@ public class SensorsActivity
     /** magnetic sensor */
     protected Sensor magnetic;
 
+    /** magnetic values */
+    protected float[] magneticValues;
+
+    /** accelerometer */
+    protected Sensor accelerometer;
+
+    /** accelerometer values */
+    protected float[] accelerometerValues;
+
+    /** orientation values */
+    protected float[] orientationValues = new float[3];
+
     /**
      * On create.
      * 
@@ -47,6 +61,8 @@ public class SensorsActivity
     {
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         magnetic = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        accelerometer =
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
 
     @Override
@@ -56,6 +72,8 @@ public class SensorsActivity
 
         sensorManager.registerListener(this, magnetic,
             SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(this, accelerometer,
+            SensorManager.SENSOR_DELAY_UI);
     }
 
     @Override
@@ -64,6 +82,7 @@ public class SensorsActivity
         super.onPause();
 
         sensorManager.unregisterListener(this, magnetic);
+        sensorManager.unregisterListener(this, accelerometer);
     }
 
     @Override
@@ -75,13 +94,51 @@ public class SensorsActivity
     public void onSensorChanged(SensorEvent event)
     {
         if (event.sensor == magnetic) {
-            EditText x = (EditText) findViewById(R.id.magneticDisplayX);
-            x.setText(Float.toString(event.values[0]));
-            EditText y = (EditText) findViewById(R.id.magneticDisplayY);
-            y.setText(Float.toString(event.values[1]));
-            EditText z = (EditText) findViewById(R.id.magneticDisplayZ);
-            z.setText(Float.toString(event.values[2]));
+            magneticValues = event.values.clone();
+
+            setText((EditText) findViewById(R.id.magneticDisplayX),
+                magneticValues[0]);
+            setText((EditText) findViewById(R.id.magneticDisplayY),
+                magneticValues[1]);
+            setText((EditText) findViewById(R.id.magneticDisplayZ),
+                magneticValues[2]);
+        } else if (event.sensor == accelerometer) {
+            accelerometerValues = event.values.clone();
+
+            setText((EditText) findViewById(R.id.accelDisplayX),
+                accelerometerValues[0]);
+            setText((EditText) findViewById(R.id.accelDisplayY),
+                accelerometerValues[1]);
+            setText((EditText) findViewById(R.id.accelDisplayZ),
+                accelerometerValues[2]);
+        }
+
+        if (magneticValues != null && accelerometerValues != null) {
+            float[] r = new float[9];
+            float[] i = new float[9];
+
+            SensorManager.getRotationMatrix(r, i, accelerometerValues,
+                magneticValues);
+
+            SensorManager.getOrientation(r, orientationValues);
+
+            setText((EditText) findViewById(R.id.orientDisplayX),
+                Math.toDegrees(orientationValues[0]));
+            setText((EditText) findViewById(R.id.orientDisplayY),
+                Math.toDegrees(orientationValues[1]));
+            setText((EditText) findViewById(R.id.orientDisplayZ),
+                Math.toDegrees(orientationValues[2]));
         }
     }
 
+    /**
+     * Set editText text value.
+     * 
+     * @param editText EditText
+     * @param value Value
+     */
+    protected void setText(EditText editText, double value)
+    {
+        editText.setText(MessageFormat.format("{0,number,#.###}", value));
+    }
 }
